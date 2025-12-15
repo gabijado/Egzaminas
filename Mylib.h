ed5953add78970c5d89f3bfdd091e57b5e57b733
gabijado <gabija.dociute@gmail.com>
2025-12-15 11:33:54 +0200

    Create Mylib.h with standard library includes
    Added header file Mylib.h with necessary includes.

---
+++ Mylib.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <cctype>
+#include <regex>
+#include <algorithm>
